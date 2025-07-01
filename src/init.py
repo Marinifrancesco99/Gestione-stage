bb59a05e1de500e5a02848b2a1154bb9d3195c3a
Marinifrancesco99 <france99marini@gmail.com>
2025-07-01 20:05:18 +0200

    Initial clean commit

---
+++ src/init.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from .config import Config
+from .routes.user_routes import user_bp
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    CORS(app)
+
+    # Importa e registra le rotte
+    
+    app.register_blueprint(user_bp, url_prefix="/api/users")
+
+    return app
+
+
